5cfe12cc87b1a651d8b546e1f96d0c79754544a4
usuario <usuario@asdfds.com>
2023-02-04 19:29:12 -0300

    ultimo commit

--- src/components/Recitales.js
+++ src/components/Recitales.js
@@ -8,63 +8,65 @@ export default function Recitales(){
            
            <ul className="Recital">
          <li>  
-       <button>
-           <p>Escuchar</p>
+      
+         <h1>Ver</h1>
     <a href='https://www.youtube.com/watch?v=66vP7Zl0ng8&embeds_euri=https%3A%2F%2Fwww.notevagustar.com%2F&feature=emb_imp_woyt'>
                 <img src="./img/verano.jpg" alt="home"/> 
    </a>
-    </button>
+    
     </li>
-    <br></br>
+    
     <li>
-	<button>
-    <p>Escuchar</p>
+	
+    <h1>Ver</h1>
     <a href='https://www.facebook.com/NTVG.OFICIALPAGE'>
                 <img src="./img/vivoRecitales.jpg" alt="Home"/> 
    </a>
-    </button>
+    
     </li>
-    <br></br>
+    
     
     <li>
-    <button>
-    <p>Escuchar</p>
+    
+    <h1>Ver</h1>
     <a href='https://www.facebook.com/NTVG.OFICIALPAGE'>
                 <img src="./img/rentvg.jpg" alt="Logo"/> 
    </a>
-    </button>
+    
     </li>
     <br></br>
 
     <li>
-    <button>
-    <p>Escuchar</p>
+    
+    <h1>Ver</h1>
     <a href='https://www.facebook.com/NTVG.OFICIALPAGE'>
                 <img src="./img/Recitalbsas.jpg" alt="Logo"/> 
    </a>
-    </button>
+    
     </li>
-    <br></br>
+    
 
     <li>
-    <button>
-    <p>Escuchar</p>
+    
+    <h1>Ver</h1>
     <a href='https://www.facebook.com/NTVG.OFICIALPAGE'>
                 <img src="./img/nuevo-show.jpg" alt="Logo"/> 
    </a>
-    </button>
+    
     </li>
-    <br></br>
+    
 
     <li>
-    <button>
-    <p>Escuchar</p>
-    <a href='https://www.facebook.com/NTVG.OFICIALPAGE'>
-                <img src="./img/lejosre.jpg" alt="Logo"/> 
-   </a>
-    </button>
+    
+      <h1>Ver</h1> 
+        <a href='https://www.facebook.com/NTVG.OFICIALPAGE'>
+   
+                <img src="./img/lejosre.jpg" alt="Logo" />
+             
+                </a>
+    
     </li>
-    <br></br>
+    
 
     </ul>
     </div>
--- src/paginas/Contacto.js
+++ src/paginas/Contacto.js
@@ -8,8 +8,8 @@ export default function Contacto(){
          
        
        
-        <Recitales />
         <Eventos />
+        <Recitales />
      
         </>
     )
