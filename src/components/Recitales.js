a2c50d8b38a913241c637d0bbe91dbc3b4256a2a
usuario <usuario@asdfds.com>
2023-02-04 00:28:31 -0300

    Responsive

--- src/components/Recitales.js
+++ src/components/Recitales.js
@@ -1,4 +1,4 @@
-
+import './Recitales.css'
 
 export default function Recitales(){
    return(
@@ -6,48 +6,67 @@ export default function Recitales(){
        <div className="Recitales">
 
            
-           <h1>Recitales</h1>
-           
+           <ul className="Recital">
+         <li>  
        <button>
            <p>Escuchar</p>
     <a href='https://www.youtube.com/watch?v=66vP7Zl0ng8&embeds_euri=https%3A%2F%2Fwww.notevagustar.com%2F&feature=emb_imp_woyt'>
                 <img src="./img/verano.jpg" alt="home"/> 
    </a>
     </button>
-   
+    </li>
+    <br></br>
+    <li>
 	<button>
     <p>Escuchar</p>
     <a href='https://www.facebook.com/NTVG.OFICIALPAGE'>
                 <img src="./img/vivoRecitales.jpg" alt="Home"/> 
    </a>
     </button>
+    </li>
     <br></br>
+    
+    <li>
     <button>
     <p>Escuchar</p>
     <a href='https://www.facebook.com/NTVG.OFICIALPAGE'>
                 <img src="./img/rentvg.jpg" alt="Logo"/> 
    </a>
     </button>
+    </li>
+    <br></br>
+
+    <li>
     <button>
     <p>Escuchar</p>
     <a href='https://www.facebook.com/NTVG.OFICIALPAGE'>
                 <img src="./img/Recitalbsas.jpg" alt="Logo"/> 
    </a>
     </button>
+    </li>
     <br></br>
+
+    <li>
     <button>
     <p>Escuchar</p>
     <a href='https://www.facebook.com/NTVG.OFICIALPAGE'>
                 <img src="./img/nuevo-show.jpg" alt="Logo"/> 
    </a>
     </button>
-    
+    </li>
+    <br></br>
+
+    <li>
     <button>
     <p>Escuchar</p>
     <a href='https://www.facebook.com/NTVG.OFICIALPAGE'>
                 <img src="./img/lejosre.jpg" alt="Logo"/> 
    </a>
     </button>
+    </li>
+    <br></br>
+
+    </ul>
     </div>
   
     )
