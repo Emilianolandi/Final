30f15d76410909f810458ef45d4c8ca5bff3afa9
usuario <usuario@asdfds.com>
2023-02-05 18:27:12 -0300

    ultimoooss

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -9,17 +9,36 @@ export default function Historia(){
          < div className='conte'>
              <br></br>
     <p className='Ress'>
-        NO TE VA GUSTAR (NTVG) se formó en Montevideo, Uruguay, en el año 1994 cuando un grupo de amigos del liceo decidieron armar una banda y presentarse en un festival en una plaza del barrio.
+        NO TE VA GUSTAR (NTVG) se formó en Montevideo, Uruguay,
+         en el año 1994 cuando un grupo de amigos del liceo decidieron 
+         armar una banda y presentarse en un festival en una plaza del barrio.
 
-Con 28 años de trayectoria, NTVG se ha convertido en una de las bandas de rock en español más convocantes de Latinoamércia. A lo largo de su carrera ha desarrollado importantes giras por el mundo, recorriendo Sudamérica entera y países como Cuba, Panamá, Costa Rica, México, Estados Unidos, Alemania, Austria, Suiza, Inglaterra y España.
+Con 28 años de trayectoria, NTVG se ha convertido en
+ una de las bandas de rock en español más convocantes
+  de Latinoamércia. A lo largo de su carrera ha desarrollado
+   importantes giras por el mundo, recorriendo Sudamérica entera
+    y países como Cuba, Panamá, Costa Rica, México, Estados Unidos,
+     Alemania, Austria, Suiza, Inglaterra y España.
 
 Sus presentaciones en vivo son poderosas, precisas y refinadas.
- Siempre con un sonido de rock, fusiona distintos géneros musicales como el reggae, el folk, la murga o el ska en base a las influencias de cada uno de sus miembros.
+ Siempre con un sonido de rock, fusiona distintos géneros 
+ musicales como el reggae, el folk, la murga o el ska en base a 
+ las influencias de cada uno de sus miembros.
 
-La banda es apasionada por los procesos de grabación y la búsqueda y exploración de sonidos nuevos en cada uno de sus álbumes. Todos sus discos han alcanzado status Oro o Platino y se han editado en Estados Unidos, España, México, Colombia, Venezuela, Perú, Chile, Uruguay y Argentina.
+La banda es apasionada por los procesos de grabación
+ y la búsqueda y exploración de sonidos nuevos en cada 
+ uno de sus álbumes. Todos sus discos han alcanzado status Oro
+  o Platino y se han editado en Estados Unidos, España, México, Colombia,
+   Venezuela, Perú, Chile, Uruguay y Argentina.
 
-Nacidos en el corazón de la encantadora ciudad de Montevideo, con su melancólica paz, la atractiva intelectualidad de sus bares y cafés, y la musicalidad permanente de cada esquina, NTVG ha construido su universo musical en base a expandir lo que le brinda su entorno con elementos que va encontrando en su intenso camino.
- Con letras sentidas e inteligentes construyen un sonido que viaja entre lo autóctono y lo cosmopolita, manteniendo un mordaz equilibrio entre la amargura existencialista y el optimismo más guerrero
+Nacidos en el corazón de la encantadora ciudad de Montevideo,
+ con su melancólica paz, la atractiva intelectualidad de sus bares
+  y cafés, y la musicalidad permanente de cada esquina, NTVG ha
+   construido su universo musical en base a expandir lo que le brinda
+    su entorno con elementos que va encontrando en su intenso camino.
+ Con letras sentidas e inteligentes construyen un sonido que viaja
+  entre lo autóctono y lo cosmopolita, manteniendo un mordaz
+   equilibrio entre la amargura existencialista y el optimismo más guerrero
  </p>
  
 <div className='Integrantes'>
